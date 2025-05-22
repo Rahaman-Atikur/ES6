831957b92128ed8d8592eb806b55a76201147397
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-05-22 21:54:34 +0100

    Arrow Function

--- arrowFunction.js
+++ arrowFunction.js
@@ -1,9 +1,13 @@
-// function total(numberOne,numberTwo){
-//     return numberOne+numberTwo;
-// }
-// console.log(total(10,20));
+function total(numberOne,numberTwo){
+    return numberOne+numberTwo;
+}
+console.log(total(10,20));
+const test = () => console.log("Hello World");
+console.log(test());
 
-//Arrow Function
- const total=(numberOne,numberTwo)=>numberOne+numberTwo;
- let result =total(24,23);
- console.log(result);
+const square = (x) => x * x;
+console.log(square(10));
+
+document.getElementById("title").addEventListener("click",(event)=>{
+    console.log("hello");
+})

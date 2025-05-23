d5a00a365ff112b1215b990c7e8e97d7f5140ebe
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-05-23 22:15:38 +0100

    Object in Loop

--- objectInLoop.js
+++ objectInLoop.js
@@ -3,6 +3,10 @@ const person = {
     age: 10,
     status:'not found'
 }
-for(let key in person){
-    console.log(`key: ${key} value ${person[key]}`);
+// for(let key in person){
+//     console.log(`key: ${key} value ${person[key]}`);
+// }
+
+for(let [key,value] of Object.entries(person)){     //Array Distraction
+    console.log(`key:${key} value:${value}`);
 }

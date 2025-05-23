4f297ac4d4b50ab82371acbc0c1c364ae47c3fa8
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-05-23 08:45:43 +0100

    Array destructuring

--- destructure.js
+++ destructure.js
@@ -1,9 +1,14 @@
 const Shop = {
   name: "Master Cafe",
-  age:30,
-  item:'Bookshop',
-  owner:'Atikur Rahaman'
+  age: 30,
+  item: "Bookshop",
+  owner: "Atikur Rahaman",
 };
-const aboutShop = {name,age,item,owner}=Shop;
+const aboutShop = ({ name, age, item, owner } = Shop);
 console.log(aboutShop.age);
 
+const shopList =['boots','Asda','Tesco','Costa','CafeNero'];
+//console.log(shopList[2]);
+// Array Destructing
+const [shopOne,shopTwo,shopThree]=shopList;
+console.log(shopThree);
